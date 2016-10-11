ecee8bb877fd3e2a6a534e7a72af2c166ac71c54
FrowserLHNS <lhns@outlook.com.br>
2016-10-10 22:02:35 -0300

    Terminando aula 3

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,7 +1,19 @@
 module.exports = function(app){
-
   app.get('/produtos', function(req,res){
-      res.render("produtos/lista");
+      var mysql = require('mysql');
+      var connection = mysql.createConnection({
+          host : "localhost",
+          user : 'root',
+          password : '',
+          database : 'casadocodigo_node.js'
+      });
+
+      connection.query('select * from livros', function(err,results){
+          res.send(results);
+      });
+      connection.end();
+
+
   });
 
 
