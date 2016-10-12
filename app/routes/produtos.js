ecbb1b2597313f535e0f46cc2f01081ffd0e6049
FrowserLHNS <lhns@outlook.com.br>
2016-10-11 23:12:36 -0300

    Aula 3 10

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,28 @@
 
 module.exports = function(app){
-    app.get('/produtos', function(req,res){
+    var listaProdutos = function(req,res){
           var connection = app.infra.connectionFactory();
-
-          connection.query('select * from produtos', function(err,results){
+          var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+          ProdutosDAO.lista(function(err,results){
               res.render('produtos/lista',{lista:results});
           });
-
       connection.end();
-  });
+  };
 
+  app.get('/produtos', listaProdutos);
+  app.get('/produtos/form', function(req, res){
+          res.render('produtos/form');
+
+  });
+  app.post('/produtos', function(req, res){
+
+    var produto = req.body;
+
+    var connection = app.infra.connectionFactory();
+    var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+    ProdutosDAO.salva(produto,function(erros, resultados){
+      res.redirect('/produtos')
+    });
+  });
 
 }
